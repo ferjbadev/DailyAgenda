5dbcd7fac10ab77b482e9feecd90d2e0b1d0c6ac
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 15:25:53 -0400

    Update add-event-dialog.tsx

--- src/components/add-event-dialog.tsx
+++ src/components/add-event-dialog.tsx
@@ -56,7 +56,7 @@ export function AddEventDialog({ open, onOpenChange, onAddEvent, selectedDate }:
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] bg-white">
         <DialogHeader>
           <DialogTitle>Agregar Evento</DialogTitle>
           <DialogDescription>Completa los detalles de tu evento</DialogDescription>
