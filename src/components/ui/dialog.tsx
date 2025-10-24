fc5a8a5bce7480a64222b69a2b16d1c1f99c24ef
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 15:51:57 -0400

    Update dialog.tsx

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -12,7 +12,7 @@ export function Dialog({ open, onOpenChange, children }: DialogProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div
-        className="fixed inset-0 bg-black/50"
+        className="fixed inset-0 bg-black/50 cursor-pointer"
         onClick={() => onOpenChange?.(false)}
       />
       <div className="relative z-50">{children}</div>
